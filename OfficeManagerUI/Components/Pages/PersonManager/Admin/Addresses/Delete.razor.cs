f086740c109dd87bc97c4b9cf489b2da1f483582
Julian Aburrow <jaburrow@btinternet.com>
2025-05-01 14:48:10 +0100

    Added Delete Address functionality and allowed hiding of columns in AddessGridviewComponent

---
+++ OfficeManagerUI/Components/Pages/PersonManager/Admin/Addresses/Delete.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace OfficeManagerUI.Components.Pages.PersonManager.Admin.Addresses;
+
+public partial class Delete
+{
+    protected override async Task OnInitializedAsync()
+    {
+        AddressModel = await AddressHandler.GetAddressAsync(AddressId);
+        MainLayout.SetHeaderValue("Delete Address");
+    }
+
+    protected override void OnInitialized()
+    {
+        MainLayout.SetBreadcrumbs(
+        [
+            GetHomeBreadcrumbItem(),
+            GetAddressHomeBreadcrumbItem(),
+            GetCustomBreadcrumbItem(DeleteTextForBreadcrumb),
+        ]);
+    }
+
+    private async Task DeleteAddress()
+    {
+        try
+        {
+            await AddressHandler.DeleteAddressAsync(AddressId);
+            Snackbar.Add($"Address {AddressModel.AddressLine1} successfully deleted", Severity.Success);
+            NavigationManager.NavigateTo($"/addresses/index/{AddressModel.PersonId}");
+        }
+        catch
+        {
+            Snackbar.Add($"An error occurred deleting {AddressModel.AddressLine1}. Please try again.", Severity.Error);
+        }
+    }
+}
--- OfficeManagerUI/Components/Shared/Components/People/AddressGridviewComponent.razor.cs
+++ OfficeManagerUI/Components/Shared/Components/People/AddressGridviewComponent.razor.cs
@@ -3,4 +3,6 @@
 public partial class AddressGridviewComponent
 {
     [Parameter] public List<AddressModel> Addresses { get; set; } = null!;
+
+    [Parameter] public bool ShowAllColumns { get; set; } = false;
 }
