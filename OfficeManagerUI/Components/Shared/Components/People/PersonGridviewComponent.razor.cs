08245bac2241faddc5383c8c2cda68ac94de0946
JulianAburrow <75375767+JulianAburrow@users.noreply.github.com>
2026-07-20 15:27:52 +0100

    Added map to list people page, changed map display (#2)
    Co-authored-by: Julian Aburrow <vdub1992@hotmail.co.uk>

--- OfficeManagerUI/Components/Shared/Components/People/PersonGridviewComponent.razor.cs
+++ OfficeManagerUI/Components/Shared/Components/People/PersonGridviewComponent.razor.cs
@@ -2,5 +2,51 @@
 
 public partial class PersonGridviewComponent
 {
+    [Inject] IJSRuntime JS { get; set; } = null!;
+
     [Parameter] public List<PersonModel> People { get; set; } = null!;
+
+    private bool ShowMap;
+
+    protected override Task OnInitializedAsync()
+    {
+        ShowMap = People
+            .SelectMany(p => p.Addresses)
+            .Any(a => a.Latitude is not null && a.Longitude is not null);
+
+        return Task.CompletedTask;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender)
+        {
+            return;
+        }
+
+        var locations = new List<object>();
+
+        foreach (var person in People)
+        {
+            foreach (var address in person.Addresses)
+            {
+                if (address.Latitude is null || address.Longitude is null)
+                {
+                    continue;
+                }
+                var addressToRender = $"{person.FirstName} {person.LastName} {address.AddressType.TypeName}: {address.AddressLine1}, {address.AddressLine2}, {address.City}, {address.Postcode}";
+                locations.Add(new
+                {
+                    lat = address.Latitude, lng = address.Longitude, popup = addressToRender
+                });
+            }
+            
+        }
+
+        if (locations.Count > 0)
+        {
+            await JS.InvokeVoidAsync("showLeafletMap", "map", locations);
+            ShowMap = true;
+        }
+    }
 }
