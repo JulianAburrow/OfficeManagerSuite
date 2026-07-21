ea7bfa8cf419a8d2f22f9dae5f4ef7d3704b4e0c
Julian Aburrow <vdub1992@hotmail.co.uk>
2026-07-21 13:55:30 +0100

    Rename file causing build error in GitHub

--- OfficeManagerUI/Components/Shared/Components/People/AddressTypeGridviewComponent_TEMP.razor.cs
+++ OfficeManagerUI/Components/Shared/Components/People/AddressTypeGridviewComponent_TEMP.razor.cs
@@ -1,6 +1,6 @@
 ﻿namespace OfficeManagerUI.Components.Shared.Components.People;
 
-public partial class AddressTypeGridviewComponent
+public partial class AddressTypeGridviewComponent_TEMP
 {
     [Parameter] public List<AddressTypeModel> AddressTypes { get; set; } = null!;
 }
