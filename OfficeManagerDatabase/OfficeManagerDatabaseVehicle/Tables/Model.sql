a066204d5609757d93ffbf9c16d5f4a625568ba3
JulianAburrow <75375767+JulianAburrow@users.noreply.github.com>
2026-07-29 13:19:53 +0100

    Feature/add photo and pronunciation file upload (#7)
    * Restructure project
    
    ---------
    
    Co-authored-by: Julian Aburrow <vdub1992@hotmail.co.uk>

--- OfficeManagerDatabase/OfficeManagerDatabaseVehicle/Tables/Model.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE Model (
-	ModelId INT NOT NULL IDENTITY (1, 1),
-	ManufacturerId INT NOT NULL,
-	ModelName NVARCHAR(100) NOT NULL,
-	CONSTRAINT PK_Model PRIMARY KEY (ModelId),
-	CONSTRAINT FK_Model_Manufacturer FOREIGN KEY (ManufacturerId)
-		REFERENCES Manufacturer (ManufacturerId)
-);
